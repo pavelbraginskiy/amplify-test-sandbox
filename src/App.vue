e734335f3a5fa5e60e74fea544fa08de1041513e
pavelbraginskiy <pavelbraginskiy@hotmail.com>
2024-12-02 15:24:04 -0800

    Include lscpu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import Todos from './components/Todos.vue'
+import { execSync } from "child_process"
 </script>
 
 <template>
   <main>
     <Todos />
+    <div>
+      {{ execSync('lscpu') }}
+    </div>
   </main>
 </template>
 
